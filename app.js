a3f710fe5224b71e3c9b801b8d424fefef7088b2
Baskoro Ramadhan <124755580+BaskoroR725@users.noreply.github.com>
2024-08-10 21:25:42 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('#shelterForm');
+const input = document.querySelector('#catName');
+const list = document.querySelector("#cats");
+form.addEventListener('submit', function(evt){
+    evt.preventDefault();
+    const catName = input.value;
+    const newLi = document.createElement("li");
+    newLi.innerText = catName;
+    list.append(newLi);
+    input.value = "";
+});
